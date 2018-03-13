29f546233ebfc0bbd90a56eb5b47948b1792b31c
Nenu011 <nenust1@outlook.com>
2018-03-13 16:27:03 -0600

    Create a EmptyActivity without xml layout.

---
+++ app/src/main/java/com/nenu011/example/splashscreen/Splash.java
@@ -0,0 +1,12 @@
+package com.nenu011.example.splashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
